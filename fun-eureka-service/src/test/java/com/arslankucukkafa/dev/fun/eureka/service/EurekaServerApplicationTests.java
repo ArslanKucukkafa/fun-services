274eefb0f7cb2d2add6538623ecc31b1e68d5f74
Arslan Kucukkafa <arslankucukkafa@gmail.com>
2025-04-14 19:40:44 +0300

    renamed services

---
+++ fun-eureka-service/src/test/java/com/arslankucukkafa/dev/fun/eureka/service/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arslankucukkafa.dev.fun.eureka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
