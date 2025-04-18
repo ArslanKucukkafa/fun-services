173b79b53bf2579f3fdae7c59637d6276227b1ee
Arslan Kucukkafa <arslankucukkafa@gmail.com>
2025-04-18 10:11:42 +0300

    feat: test commit + workflow settings make valid for all branches

---
+++ fun-auth-service/src/main/java/com/arslankucukkafa/dev/funauthservice/FunAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.arslankucukkafa.dev.funauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunAuthServiceApplication.class, args);
+	}
+
+}
--- fun-gateway-service/src/main/java/com/arslankucukkafa/dev/gatewayservice/config/ServiceConfigurations.java
+++ fun-gateway-service/src/main/java/com/arslankucukkafa/dev/gatewayservice/config/ServiceConfigurations.java
@@ -2,5 +2,18 @@ package com.arslankucukkafa.dev.gatewayservice.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ServiceConfigurations {
+
+    Queue<String> serviceUrls = new LinkedList<>();
+
+    public Queue<String> getServiceUrls() {
+
+        serviceUrls.remove("");
+
+        return serviceUrls;
+    }
+
 }
